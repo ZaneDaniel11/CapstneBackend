bcc01df283d0b0c923595074061546a076843972
Zane Daniel <118499857+ZaneDaniel11@users.noreply.github.com>
2025-02-27 16:10:26 +0800

    Update Asset Status

--- Backend/obj/Debug/net8.0/Daper.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Daper.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Daper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f79c52eb3132c73089543e97cbc24319b9768fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3d7f258f0176709cbe2c0df286a3c255e7caf07")]
 [assembly: System.Reflection.AssemblyProductAttribute("Daper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Daper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
