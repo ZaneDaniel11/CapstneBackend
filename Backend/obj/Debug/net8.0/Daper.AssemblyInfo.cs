734ac6e295f46be0442caaf3dfa5fdad63fbedba
Zane Daniel <118499857+ZaneDaniel11@users.noreply.github.com>
2025-02-28 16:24:53 +0800

    Created table for Asset Historical Analysis

--- Backend/obj/Debug/net8.0/Daper.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Daper.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Daper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3d7f258f0176709cbe2c0df286a3c255e7caf07")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b0b9dd341caa3eb8d73d757e9eb866e0ac53d75")]
 [assembly: System.Reflection.AssemblyProductAttribute("Daper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Daper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
