e0e3a98c604b53cd57cbf9bef3fcd659ae36e2d4
Zane Daniel <118499857+ZaneDaniel11@users.noreply.github.com>
2025-04-23 15:58:49 +0800

    Update

--- Backend/obj/Debug/net8.0/Daper.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Daper.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Daper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8228b817b4cdb565c8534e711f818905fe5bd0bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e3a9657ecebef80d901b85231ef48bacb16e84a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Daper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Daper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
